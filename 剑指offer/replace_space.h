794ecf3b15fc2e1147ca9b79f51b6d27f27170ca
iwin <iwiniwin@foxmail.com>
2023-02-21 20:29:00 +0800

    add ReplaceSpace problem

---
+++ 剑指offer/replace_space.h
@@ -0,0 +1,39 @@
+/*
+题目名称：
+替换空格
+
+题目描述：
+请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+
+代码结构：
+class Solution {
+public:
+    string replaceSpace(string s) {
+        // write code here
+    }
+};
+*/
+
+#ifndef ALGORITHM_CPP_REPLACE_SPACE_H
+#define ALGORITHM_CPP_REPLACE_SPACE_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace ReplaceSpace {
+    class Solution {
+    public:
+        string ReplaceSpace(string s) {
+            // write code here
+        }
+
+        void Test() {
+            std::cout << "test" << endl;
+        }
+    };
+}
+
+
+#endif //ALGORITHM_CPP_REPLACE_SPACE_H
