460b2e2a02474a2d76261d07a87a27dfb1c21153
iwin <iwiniwin@foxmail.com>
2023-02-22 15:57:04 +0800

    add the solution to the ReConstructBinaryTree

---
+++ common.h
@@ -0,0 +1,58 @@
+//
+// Created by IWIN on 2023/2/22.
+//
+
+#ifndef ALGORITHM_CPP_COMMON_H
+#define ALGORITHM_CPP_COMMON_H
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+
+    ListNode(int x) :
+            val(x), next(NULL) {
+    }
+};
+
+void print(vector<int> list) {
+    for (auto it = list.begin(); it != list.end(); it++) {
+        std::cout << *it << " ";
+    }
+    std::cout << endl;
+}
+
+void print(int val) {
+    std::cout << val << endl;
+}
+
+void print(TreeNode *head) {
+    queue<TreeNode*> nodes;
+    nodes.push(head);
+    while(!nodes.empty()) {
+        TreeNode *node = nodes.front();
+        nodes.pop();
+        if(node != NULL) {
+            std::cout << node->val << " ";
+            nodes.push(node->left);
+            nodes.push(node->right);
+        } else {
+            std::cout << "# ";
+        }
+    }
+    std::cout << endl;
+}
+
+
+#endif //ALGORITHM_CPP_CONSOLE_H
--- 剑指offer/re_construct_binary_tree.h
+++ 剑指offer/re_construct_binary_tree.h
@@ -21,31 +21,61 @@ public:
     a
    b c
  d e f g
+1. 递归 按子树处理。想要截取列表子序列时，可以思考用索引代替，从而避免重新创建列表
+2. 想不到还有什么解法了
 */
 
 #ifndef ALGORITHM_CPP_RE_CONSTRUCT_BINARY_TREE_H
 #define ALGORITHM_CPP_RE_CONSTRUCT_BINARY_TREE_H
-#include <iostream>
-#include <vector>
-using namespace std;
+
+#include "../common.h"
 
 namespace ReConstructBinaryTree {
-    struct TreeNode {
-        int val;
-        TreeNode *left;
-        TreeNode *right;
-
-        TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-    };
 
     class Solution {
     public:
-        TreeNode *reConstructBinaryTree(vector<int> pre, vector<int> vin) {
-
+        /*
+         * 解法
+         * 基本思路：
+         * 前序遍历是先根节点再左右节点。所以前序序列的第一个节点一定是根节点
+         * 中序遍历是先左节点，再根节点，再右节点。那么在中序序列中找到根节点以后，
+         * 根节点左边的元素为左子树中序序列，根节点右边的元素为右子树中序序列
+         * 根据左右子树的元素数量，可以再在前序序列中分别找出左右子树的前序序列
+         * 再递归重构左右子树即可
+         * 举个例子，比如中序序列{4,7,2,1,5,3,8,6}，前序序列{1,2,4,7,3,5,6,8}，根节点是1。
+         * 那么A的左子树是由1左边的中序序列{4,7,2}构成的二叉树A1，
+         * A的右子树是由1右边的中序序列{5,3,8,6}构成的二叉树A2。
+         * 因为A1的中序序列是{4,7,2}，元素个数是3，所以A的前序序列中，
+         * 根节点1后面的三个元素即{2,4,7}就是A1的前序序列。
+         * 所以A2的前序序列就是{2,4,7}后面的所有元素，即{3,5,6,8}
+         * 到现在，我们已经把已知A的前序序列和中序序列求A的问题，转换为已知A1的前序中序序列求A1，已知A2的前序中序序列求A2的问题。
+         * 继续递归下去，就可以重建出这颗二叉树了。
+         * 前中后序遍历介绍 https://www.cnblogs.com/iwiniwin/p/10793652.html
+         */
+        TreeNode* ReConstructBinaryTree(vector<int> pre, vector<int> vin) {
+            return ReConstructBinaryTree(pre, 0, pre.size() - 1, vin, 0, vin.size() - 1);
         }
-        
+
+        TreeNode* ReConstructBinaryTree(vector<int> &pre, int pre_left, int pre_right, vector<int> &vin, int vin_left, int vin_right) {
+            if(pre_left > pre_right || vin_left > vin_right) return NULL;
+            TreeNode *head = new TreeNode(pre[pre_left]);
+            for(int i = vin_left; i <= vin_right; i ++) {
+                if(vin[i] == head->val) {
+                    head->left = ReConstructBinaryTree(pre, pre_left + 1, pre_left + i - vin_left, vin, vin_left, i - 1);
+                    head->right = ReConstructBinaryTree(pre, pre_left + i - vin_left + 1, pre_right, vin, i + 1, vin_right);
+                }
+            }
+            return head;
+        }
+
         void Test() {
-            std::cout << "test" << endl;
+            vector<int> pre = {1, 2, 4, 7, 3, 5, 6, 8};
+//            pre = {1};
+//            pre = {1, 2, 3, 4, 5, 6, 7};
+            vector<int> vin = {4, 7, 2, 1, 5, 3, 8, 6};
+//            vin = {1};
+//            vin = {3, 2, 4, 1, 6, 5, 7};
+            print(ReConstructBinaryTree(pre, vin));
         }
     };
 }
