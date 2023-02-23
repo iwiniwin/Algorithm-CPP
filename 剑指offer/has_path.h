620a790f44d2daf67638e3b7ba09f072e5cdaaec
iwin <iwiniwin@foxmail.com>
2023-02-23 15:01:42 +0800

    add the solution to the HasPath

--- 剑指offer/has_path.h
+++ 剑指offer/has_path.h
@@ -17,21 +17,65 @@ public:
         // write code here
     }
 };
+
+想法：
+递归，遍历substring，在矩阵中记录是否走过
 */
 
 #ifndef ALGORITHM_CPP_HAS_PATH_H
 #define ALGORITHM_CPP_HAS_PATH_H
+
 #include "../common.h"
 
 namespace HasPath {
     class Solution {
     public:
-        bool hasPath(vector<vector<char> >& matrix, string word) {
-            // write code here
+        /*
+         * 解法
+         * 基本思路：
+         * 分别以矩阵的每个元素为起点，判断路径是否存在
+         * 从起点开始按上下左右的顺序搜索，已经经过的元素标记为true
+         * 如果此路不通，则回溯到上一层，并还原标记位
+         */
+        bool hasPath(vector<vector<char> > &matrix, string word) {
+            if (matrix.size() == 0 || matrix[0].size() == 0) return false;
+            vector<vector<bool>> record(matrix.size(), vector<bool>(matrix.size(), false));
+            for (int i = 0; i < matrix.size(); i++) {
+                for (int j = 0; j < matrix[i].size(); j++) {
+                    if (hasPath(matrix, i, j, record, word, 0)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        bool
+        hasPath(vector<vector<char>> &matrix, int row, int col, vector<vector<bool>> &record, string word, int offset) {
+            if (offset >= word.length()) return true;
+            if (row >= matrix.size() || col >= matrix[row].size()) return false;
+            if (matrix[row][col] == word[offset] && !record[row][col]) {
+                record[row][col] = true;
+                bool ret = hasPath(matrix, row + 1, col, record, word, offset + 1) ||
+                           hasPath(matrix, row - 1, col, record, word, offset + 1) ||
+                           hasPath(matrix, row, col + 1, record, word, offset + 1) ||
+                           hasPath(matrix, row, col - 1, record, word, offset + 1);
+                record[row][col] = false;
+                return ret;
+            }
+            return false;
         }
 
         void Test() {
-            print("test");
+            vector<vector<char>> matrix = {
+                    {'a', 'b', 'c', 'e'},
+                    {'s', 'f', 'c', 's'},
+                    {'a', 'd', 'e', 'e'}
+            };
+//            matrix = {};
+            string word = "abcced";
+//            word = "abcb";
+            print(hasPath(matrix, word));
         }
     };
 }
